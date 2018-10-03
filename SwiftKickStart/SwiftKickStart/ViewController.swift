59705b46095145831770bc5578a3e53f75478734
Aashish Tyagi <aashish.tyagi@crmnext.in>
2018-10-03 16:20:57 +0530

    Intial commit

---
+++ SwiftKickStart/SwiftKickStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftKickStart
+//
+//  Created by MOBILE MAC1 on 10/3/18.
+//  Copyright © 2018 MOBILE MAC1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ SwiftKickStart/SwiftKickStart/ViewControllers/StackView/StackViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewController.swift
+//  SwiftKickStart
+//
+//  Created by MOBILE MAC1 on 10/3/18.
+//  Copyright © 2018 MOBILE MAC1. All rights reserved.
+//
+
+import UIKit
+
+class StackViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      addSlideMenuButton()
+    }
+}
